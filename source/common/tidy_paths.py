90d53bcb58a665cc068d8e7c1b4883e04ab22a76
OllyButters <olly.butters@bristol.ac.uk>
2014-04-12 13:46:35 +0100

    added routine to tidy old data away

---
+++ source/common/tidy_paths.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+#get rid of any old temp data sitting around
+
+import os
+
+import ds_config
+
+
+#Get rid of the temp dir if it exists                                                                                                                                                               
+if (os.path.exists(ds_config.root_dir+'/temp') == True):
+    shutil.rmtree(ds_config.root_dir+'/temp')
+
+#Make the temp dir and its sub dirs.                                                                                                                                                                
+os.mkdir(ds_config.root_dir+'/temp')
+os.mkdir(ds_config.root_dir+'/temp/A')
+os.mkdir(ds_config.root_dir+'/temp/B')
+os.mkdir(ds_config.root_dir+'/temp/client')
