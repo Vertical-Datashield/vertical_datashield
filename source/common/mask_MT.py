a68544007a1d3c260133d5b726979b2229da0637
OllyButters <olly.butters@bristol.ac.uk>
2014-04-12 11:43:36 +0100

    fixed locations

--- source/common/mask_MT.py
+++ source/common/mask_MT.py
@@ -27,8 +27,7 @@ print "Saving to: "+masked_data_path_local
 #fn(masking vector path, data set path, output path)
 cmd = 'Rscript '+ds_config.source_dir+biobank_name+'/mask_MT.R '
 cmd += ds_config.temp_dir+biobank_name+'/'+masking_vector_name+' '
-cmd += ds_config.data_dir+
-biobank_name+'/'+data_set_name+' '
+cmd += ds_config.data_dir+biobank_name+'/'+data_set_name+' '
 cmd += masked_data_path_local
 os.system(cmd)
 
@@ -44,9 +43,8 @@ if ds_config.local_only == True:
     shutil.copyfile(masked_data_path_local,masked_data_path_remote)
 else:
     #Do something
-    print "DEFINATELY WRONG!"
-    cmd = 'scp '+masking_vector_path_client+' '+ds_config.remote_settings[biobank_name,'username']+'@'+ds_config.remote_settings[biobank_name,'ip_address']+':'+masking_vector_path_remote
+    cmd = 'scp '+masked_data_path_local+' '+ds_config.remote_settings[biobank_name,'username']+'@'+ds_config.remote_settings[biobank_name,'ip_address']+':'+masked_data_path_remote
     print cmd
-
+    os.system(cmd)
 
 print "Finished masking "+data_set_name+"\n"
