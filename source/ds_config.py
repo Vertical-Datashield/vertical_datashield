7b9dfecfdb24436330257cd8acfbe00e75d5cd17
OllyButters <olly.butters@bristol.ac.uk>
2014-04-12 10:51:40 +0100

    added vm config

--- source/ds_config.py
+++ source/ds_config.py
@@ -4,7 +4,8 @@ import os
 
 #Set up the directory paths
 #root_dir=os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-root_dir="/home/ob13747/vertical_datashield/vertical_datashield/trunk"
+#root_dir="/home/ob13747/vertical_datashield/vertical_datashield/trunk"
+root_dir="/home/vds/vertical_datashield/trunk"
 source_dir=root_dir+"/source/"
 data_dir=root_dir+"/data/"
 temp_dir=root_dir+"/temp/"
