49bc0fe5724e0fd9bb6d2340636aa23698c31824
ollybutters <vds@dstesting-102-v2-32bit.(none)>
2014-04-26 16:44:22 +0100

    dropped row names
    Signed-off-by: ollybutters <vds@dstesting-102-v2-32bit.(none)>

--- source/common/summary_M.R
+++ source/common/summary_M.R
@@ -23,7 +23,10 @@ a<-read.csv(data_matrix_in, header=TRUE)
 
 num_rows <- nrow(a)
 num_cols <- ncol(a)
-summary=rbind(num_rows,num_cols)
+
+col_names <- colnames(a)
+
+summary=rbind(num_rows,num_cols,col_names)
 
 #print(sums)
 
